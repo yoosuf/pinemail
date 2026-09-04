f942cf18e0f60f7a76ccbab1fc34a6e4c3113ef3
yoosuf <mayoosuf@gmail.com>
2026-09-04 13:13:20 +0530

    chore: remove tracked dist files from git cache and delete local dist

--- dist/choco/tools/chocolateyuninstall.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$packageName = 'pinemail'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName 'pinemail-v0.1.0-x86_64-pc-windows-msvc.zip'
